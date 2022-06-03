26a4236df59d4c3011e3ab76dadba63cab40f48e
Michael Stallings <stall84@gmail.com>
2022-06-03 12:13:14 -0400

    EXPRESS-UNDERLYING-REQ-OBJ: Committing just to show how to access the underlying Express Request object. Going forward will remove and use Nest's decoupled decorators to allow for future switching of underlying server framework (e.g. Fastify, etc)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 // Nest allows the Controller decorator take in the prefix route.. so in our case the route/methods below like signup/signin will all be prefixed
 // by www.whatever.com/auth/
@@ -11,8 +12,11 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Req() req: Request) {
+    // The underlying Express Request object can be accessed via the @Req() decorator..  We will be removing this and
+    // Using instead NestJS's @Body() decorator after this commit since it is decoupled from the underlying framework,
+    // In case for instance we wanted to change from Express to Fastify NestJS
+    return this.authService.signup(req);
   }
   @Post('signin')
   signin() {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
+import { Request } from 'express';
 import { DbService } from 'src/dbService/dbService.service';
 
 @Injectable()
 export class AuthService {
   constructor(private dbService: DbService) {}
-  signin() {
-    return {
-      msg: 'I have signed in..',
-    };
-  }
-
-  signup() {
+  signup(req: Request) {
+    console.log(req.headers);
     // Nest will 'coerce' and format the return datatype for you. Below would be same as
     // res.send(200).json(...)
     return {
       msg: 'Hello. You are signing up!',
+    };
+  }
+
+  signin() {
+    return {
+      msg: 'I have signed in..',
     };
   }
 }
